982378d997f936a916360201391b6b330ae46516
felipessantosbsa-cypher <felipessantosbsa@gmail.com>
2026-03-15 18:31:21 -0300

    feat: adiciona upload de PDF/DOCX e página de status dos contratos

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template, request
-from database import create_tables, insert_contract, get_all_contracts
+from flask import Flask, render_template, request, send_file
+from database import create_tables, insert_contract, get_all_contracts, DATABASE_NAME
+import sqlite3
+import os
+
 app = Flask(__name__)
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'pdf', 'docx', 'doc'}
 
 @app.route("/")
 def way_route():
@@ -10,15 +16,27 @@ def way_route():
 def cadastrar():
     if request.method == "POST":
         client = request.form.get("client")
+        cpf_client = request.form.get("cpf_client")
         local = request.form.get("local")
         cep = request.form.get("cep")
         rent_value = request.form.get("rent_value")
         entry_date = request.form.get("entry_date")
         email_user = request.form.get("email_user")
+        file_upload = request.files.get("file_upload")
+
+        file_path = None
+        if file_upload and file_upload.filename != "":
+            filename = file_upload.filename
+
+            filepath = os.path.join(UPLOAD_FOLDER, filename)
+            file_upload.save(filepath)
+            
+            file_path = filepath
+            print(f"arquivo salvo em >>> {filepath}")
 
         print(f"Cliente: {client}")
 
-        insert_contract(client, local, cep, rent_value, entry_date, email_user)
+        insert_contract(client, cpf_client, local, cep, rent_value, entry_date, email_user, file_path)
         return("contrato cadastrado com sucesso!")
     
     return render_template("cadastrar.html")
@@ -27,6 +45,25 @@ def cadastrar():
 def listar():
     contracts = get_all_contracts()
     return render_template("listar.html", contracts=contracts)
+
+@app.route("/status")
+def view_status():
+    contracts = get_all_contracts()
+    return render_template("status.html", contracts=contracts)
+
+@app.route("/download/<int:contract_id>")
+def dowload_file(contract_id):
+    conn = sqlite3.connect(DATABASE_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT file_path FROM contracts WHERE id = ?", (contract_id,))
+    result = cursor.fetchone()
+    conn.close()
+    if result and result[0]: 
+        return send_file(result[0])
+    else:
+        return "PDF não encontrado!"
+
 
 if __name__ == "__main__":
     create_tables()
--- database.py
+++ database.py
@@ -1,4 +1,6 @@
 import sqlite3
+import os
+
 DATABASE_NAME = "contracts.db"
 
 def create_tables():
@@ -8,24 +10,30 @@ def create_tables():
         CREATE TABLE IF NOT EXISTS contracts (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             client_name TEXT NOT NULL,
+            cpf_client TEXT NOT NULL,
             location TEXT NOT NULL,
             cep TEXT,
             rent_value REAL NOT NULL,
             entry_date DATE NOT NULL,
-            email TEXT
+            email TEXT,
+            file_path TEXT
         )       
     """)
     conn.commit()
     conn.close()
+
+    if not os.path.exists("uploads"):
+        os.makedirs("uploads")
+        print("pasta feita!")
     
-def insert_contract(client_name, location, cep, rent_value, entry_date, email):
+def insert_contract(client_name, cpf_client, location, cep, rent_value, entry_date, email, file_path):
     conn = sqlite3.connect(DATABASE_NAME)
     cursor = conn.cursor()
 
     cursor.execute("""
-        INSERT INTO contracts (client_name, location, cep, rent_value, entry_date, email)
-        VALUES (?, ?, ?, ?, ?, ?)
-        """, (client_name, location, cep, rent_value, entry_date, email))
+        INSERT INTO contracts (client_name, cpf_client, location, cep, rent_value, entry_date, email, file_path)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        """, (client_name, cpf_client, location, cep, rent_value, entry_date, email, file_path))
     conn.commit()
     conn.close()
 
